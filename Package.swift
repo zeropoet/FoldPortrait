b3bfcc48010692aaec012b1ec6bce8e41a087ca5
Zeropoet <mancel@zeropoet.xyz>
2026-08-30 20:23:29 -0400

    Pin canonical FoldKernel package

--- Package.swift
+++ Package.swift
@@ -19,7 +19,10 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(path: "../FoldKernel"),
+        .package(
+            url: "https://github.com/zeropoet/FoldKernel.git",
+            exact: "1.0.1"
+        ),
         .package(url: "https://github.com/swiftlang/swift-testing.git", revision: "swift-6.1.2-RELEASE"),
     ],
     targets: [
